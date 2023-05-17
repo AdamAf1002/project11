8f6f9867bc7c58b69f38d1d25d15c156e57471af
AdamAf1002 <62314797+AdamAf1002@users.noreply.github.com>
2023-05-17 14:52:18 +0200

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,31 +2,50 @@
 
 namespace App\Controller;
 
-use App\Entity\Etudiant;
 use App\Entity\User;
-use App\Repository\EtudiantRepository;
 use App\Repository\UserRepository;
+use App\Repository\FiliereRepository;
+use App\Repository\GroupeRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {    
     /**utlisateur courent 
     private User $curentuser;*/
 
-    #[Route(path:['/','/home'], name: 'app_home',methods:['GET','POST'])]
-    public function index(Request $request): Response
+    #[Route(path:['/','/home'], name: 'home',methods:['GET','POST'])]
+    public function index(FiliereRepository $filiereRepository,UserRepository $userRepository,GroupeRepository $groupeRepository): Response
       
     {
        
-        if(!$request->getSession()->get("user"))
+        if(!$this->getUser())
         return $this->redirectToRoute('security.login');
-        
-                
+
+         $currentuser=null;
+        foreach ($userRepository->findAll() as $key => $value) {
+            if($value->getEmail()==$this->getUser()->getUserIdentifier()){
+                $currentuser=$value;
+            }
+        }
+
+        $filieres=[];
+        foreach ($filiereRepository->findAll() as $filiere) {
+            array_push($filieres,$filiere->getNomfiliere());
+        }
+
+        $groupes=[];
+
+        foreach ($groupeRepository->findAll() as $groupe) {
+            array_push($groupes,$groupe->getNomgrp());
+        }
         return $this->render('home/acceuil.html.twig', [
             'controller_name' => 'FiliereController'
+            ,'filieres'=>$filieres
+            ,'groupes'=>$groupes
+            , 'username'=>$currentuser->getPrenom()
+
         ]);
     }
 
